0451fc914926f89c1bfb9c553cb8d4221a8b9589
Dwip Lahare <123765492+Dwip272004@users.noreply.github.com>
2024-10-11 22:11:05 +0530

    update app.py

--- app.py
+++ app.py
@@ -348,6 +348,6 @@ def delete_post(post_id):
     return jsonify({'success': False, 'message': 'Post not found or permission denied.'}), 404
 
 
-# Run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the port from the environment variable, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
