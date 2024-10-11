e46b70c01ef1d299c283961a5dc20f8b7591b9f6
Dwip Lahare <123765492+Dwip272004@users.noreply.github.com>
2024-10-11 22:59:50 +0530

    xvxc

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'iamdwip'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+        'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'misfits.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or 'uploads'
