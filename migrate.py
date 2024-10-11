270418522d4911efbdf3fe96c4c400e9de924839
Dwip Lahare <123765492+Dwip272004@users.noreply.github.com>
2024-10-11 22:52:27 +0530

    dsfdf

--- migrate.py
+++ migrate.py
@@ -6,8 +6,4 @@ migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     migrate.init_app(app, db)
-<<<<<<< HEAD
     migrate.upgrade()
-=======
-    migrate.upgrade()
->>>>>>> ce476617c14f7e08fc958afd74510ff9685b921a
