ce476617c14f7e08fc958afd74510ff9685b921a
Dwip Lahare <123765492+Dwip272004@users.noreply.github.com>
2024-10-11 22:48:42 +0530

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -1,6 +1,9 @@
 import os
 from app import app, db
+from flask_migrate import Migrate
+
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    migrate.init_app(app, db)
+    migrate.upgrade()
